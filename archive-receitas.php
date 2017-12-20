bf38ff7c5d0ab9e8cda0404965f220a1848c2ed5
Elvis B. Martins <elvisbmartins@gmail.com>
2017-12-20 18:50:38 -0200

    refactor archives-receitas, adicionado paginação com filter no function, adicionado loop para adicioar post e completar a lista #1

--- archive-receitas.php
+++ archive-receitas.php
@@ -1,69 +1,53 @@
 <?php get_header(); ?>
+
 <div class="banner-mobile-receita"></div>
+
 <?php layerslider(4) ?>
+
 <div class="faixa title_receita" style="font-family: Arial;">
-			<div class="container cozinha-receita" style="font-family: Arial;">
-				<h2>Confira abaixo as receitas assinadas pelos nossos Chefs!</h2>
-			</div>
-		</div>
+	<div class="container cozinha-receita" style="font-family: Arial;">
+		<h2>Confira abaixo as receitas assinadas pelos nossos Chefs!</h2>
+	</div>
+</div>
+
 <section class="container-fluid receitas">
 	<div class="filtr-container">
-
-<?
-$posts = get_posts(array(
-'posts_per_page'	=> 15,
-'post_type'	=> 'receitas'
-));
-if( $posts ): ?>
-
-		<?php foreach( $posts as $post ): setup_postdata( $post );?>
-		<style type="text/css">
-		.<?php global $post;
-			echo $post->post_name;
-			?> {
-				background-image: url("<?php echo get_the_post_thumbnail_url() ?>");
-				background-position: center;
-				background-repeat: none;
-			}
-		}
-		</style>
-
-		<?php
-		$terms = get_the_terms( $post->ID, 'hoteis_restaurantes' );
-		if ( $terms ) {
-			foreach ( $terms as $term ) {
-				$term_id = $term->term_id;
-			}
-		}
-		?>
-          <?php $aguarde = get_field('aguarde');
-		
-		if( $aguarde && in_array('sim', $aguarde)) { ?>
-		<div class="aguarde_item col-lg-4 col-md-4 col-sm-6 filtr-item <?php global $post; echo $post->post_name; ?> " data-category="<?php echo $term_id ?>">
-			<a>
-						<div class="resumobeneficio">
-				<h2><?php echo the_title() ?></h2>
-				
+	<?php
+		$count_multiplo = 0;
+		$count_posts = wp_count_posts();
+		while ( have_posts() ) : the_post();
+	?>
+		<div class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php the_title(); ?>" style="background-image: url('<?php echo get_the_post_thumbnail_url();?>')">
+			<a href="<?php the_title(); ?>">
+				<div class="resumobeneficio">
+					<h2><?php the_title(); ?></h2>
 				</div>
 			</a>
 		</div>
-		<? } else{ ?> 
-			<div class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php global $post; echo $post->post_name; ?> " data-category="<?php echo $term_id ?>">
-			<a href="<?php echo get_permalink()?>">
-						<div class="resumobeneficio">
-				<h2><?php echo the_title() ?></h2>
-				
-				</div>
-			</a>
-		</div>
-<?php } ?>
-		<?php endforeach; ?>
-	
-
-
+	<?php 
+		$count_multiplo++;
+		endwhile; 
+	?>
+	<?php 
+		$htmlEspaco = '<div class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php the_title(); ?>" style="background-image: url(http://cookloversleclub.com.br/wp-content/themes/twentyseventeen/img/thumb_receitas.jpg)"><a href="<?php the_title(); ?>"><div class="resumobeneficio"><h2>Aguarde</h2></div></a></div>';
+		$count_multiplo = $count_multiplo % 3;
+		if( $count_multiplo == 1 ){
+			echo $htmlEspaco . $htmlEspaco;
+		}if($count_multiplo == 2){ 
+			echo $htmlEspaco;
+		};
+	?>
 	</div>
-
-	<?php endif; ?>
 </section>
-
+<aside id="paginacao" class="container-fluid faixa">
+	<div class="container ">
+		<?php
+			the_posts_pagination( array(
+				'mid_size'  => 5,
+				'prev_text' => __( 'Anterior', 'textdomain' ),
+				'next_text' => __( 'Próxima', 'textdomain' ),
+			) );
+		?> 
+	</div>
+</aside>
 <?php get_footer();?>

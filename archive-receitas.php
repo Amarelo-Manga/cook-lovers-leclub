94d71a674faf4c448c76d27671bd2f1e5b3412c4
Elvis B. Martins <elvisbmartins@gmail.com>
2017-12-21 15:01:10 -0200

    archive receitas

--- archive-receitas.php
+++ archive-receitas.php
@@ -14,7 +14,6 @@
 	<div class="filtr-container">
 	<?php
 		$count_multiplo = 0;
-		$count_posts = wp_count_posts();
 		while ( have_posts() ) : the_post();
 	?>
 		<div class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php the_title(); ?>" style="background-image: url('<?php echo get_the_post_thumbnail_url();?>')">
@@ -30,7 +29,7 @@
 	?>
 	<?php 
 		$htmlEspaco = '<div class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php the_title(); ?>" 
-						style="background:url(/wp-content/themes/twentyseventeen/img/thumb_receitas.jpg)"><a href="#" >"><div class="resumobeneficio"><h2>Aguarde</h2></div></a></div>';
+						style="background:url(/wp-content/themes/twentyseventeen/img/thumb_receitas.jpg)"><a href="#" ><div class="resumobeneficio"><h2>Aguarde</h2></div></a></div>';
 		$count_multiplo = $count_multiplo % 3;
 		if( $count_multiplo == 1 ){
 			echo $htmlEspaco . $htmlEspaco;

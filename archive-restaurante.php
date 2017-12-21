189e967c8de9b1cd5e0e01e27632ce5616c65c9d
Elvis B. Martins <elvisbmartins@gmail.com>
2017-12-21 15:01:50 -0200

    refactor archive-restaurante e adicionado filtro de estado

--- archive-restaurante.php
+++ archive-restaurante.php
@@ -11,83 +11,61 @@ do Cook Lovers e os benefícios que eles oferecem<br> para nossos associados.</p
 </section>
 
 <nav class="faixa salmao">
-
-	<?php //$taxonomy = 'hoteis_restaurantes'; $tax_terms = get_terms( $taxonomy );?>
-
-	<!--ul class="simplefilter">
-		<li class="active" data-filter="all">All</li>
-		<?php // foreach ( $tax_terms as $tax_term ) {echo '<li data-filter="' . $tax_term->term_id. '" class="">' . $tax_term->name . '</li>';}?>
-
-	</ul-->
+	<div id="filtroestado" class="container">
+		<span>Selecione o estado: </span>
+		<select class="select-estado">
+			<option value='all'>Selecione</option>
+			<option value='all'>Todos</option>
+		<?php 
+			$estadosBrasileiros = array('ac'=>'Acre','al'=>'Alagoas','ap'=>'Amapá','am'=>'Amazonas','ba'=>'Bahia','ce'=>'Ceará','df'=>'Distrito Federal','es'=>'Espírito Santo','go'=>'Goiás','ma'=>'Maranhão','mt'=>'Mato Grosso','ms'=>'Mato Grosso do Sul','mg'=>'Minas Gerais','pa'=>'Pará','pb'=>'Paraíba','pr'=>'Paraná','pe'=>'Pernambuco','pi'=>'Piauí','rj'=>'Rio de Janeiro','rn'=>'Rio Grande do Norte','rs'=>'Rio Grande do Sul','ro'=>'Rondônia','rr'=>'Roraima','sc'=>'Santa Catarina','sp'=>'São Paulo','se'=>'Sergipe','to'=>'Tocantins');
+			while ( have_posts() ) : the_post();
+				if( get_field('estado') ) {
+				   $estPost[] = get_field('estado');
+				} 
+			endwhile;
+			$estPost = array_unique($estPost);
+			natcasesort($estPost);
+			foreach ( $estPost as $v) {
+				echo "<option value='".$v."'>".$estadosBrasileiros[$v]."</option>";
+			}
+		?>
+		</select>
+	</div>
 </nav>
-
-
-
 
 <section class="container-fluid restaurante">
 	<div class="filtr-container">
-
-<?
-$posts = get_posts(array(
-'posts_per_page'	=> -1,
-'post_type'	=> 'restaurante'
-));
-if( $posts ): ?>
-
-		<?php foreach( $posts as $post ): setup_postdata( $post );?>
-		<style type="text/css">
-		.<?php global $post;
-			echo $post->post_name;
-			?> {
-				background-image: url("<?php echo get_the_post_thumbnail_url() ?>");
-				background-position: center;
-				background-repeat: none;
-			}
-		}
-		</style>
-
-		<?php
-		$terms = get_the_terms( $post->ID, 'hoteis_restaurantes' );
-		if ( $terms ) {
-			foreach ( $terms as $term ) {
-				$term_id = $term->term_id;
-			}
-		}
-		?>
-		       <?php $aguarde = get_field('aguarde');
-		
-		if( $aguarde && in_array('sim', $aguarde)) { ?>
-		
-		<div class="col-lg-4 col-md-6 col-sm-12 filtr-item <?php global $post; echo $post->post_name; ?> " data-category="<?php echo $term_id ?>">
-			
-			<? if (get_field('post_de_aguarde') != 'sim') { $link = get_permalink()."/?am_force_theme_layout=desktop";}?>
-			
-			<?php $icon = get_field('icone_promocao'); ?>
-				<a>
-			<?php if ($icon){ ?><img src="<?php echo  $icon['url'] ?>" alt="<?php echo  $icon['alt']?>" class="icon_rest_promo"/><?php } ?>
-			<div class="resumobeneficio">
-				<h2><?php the_title() ?></h2>
-				<h3><?php the_field('nome_completo_do_hotel') ?></h3>
-				</div>
-		</a>
-	</div>
-		<?php }else{?> 
-				<div class="col-lg-4 col-md-6 col-sm-12 filtr-item <?php global $post; echo $post->post_name; ?> " data-category="<?php echo $term_id ?>">
-			
-			<? if (get_field('post_de_aguarde') != 'sim') { $link = get_permalink()."/?am_force_theme_layout=desktop";}?>
-			<a href="<?php echo $link ?>">
-			<?php $icon = get_field('icone_promocao'); ?>
-			<?php if ($icon){ ?><img src="<?php echo  $icon['url'] ?>" alt="<?php echo  $icon['alt']?>" class="icon_rest_promo"/><?php } ?>
-			<div class="resumobeneficio">
-				<h2><?php the_title() ?></h2>
-				<h3><?php the_field('nome_completo_do_hotel') ?></h3>
+	<?php
+		$count_multiplo = 0;
+		while ( have_posts() ) : the_post();
+			$estado = get_field('estado');
+	?>
+		<div  class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php the_title(); ?>" style="background-image: url('<?php echo get_the_post_thumbnail_url();?>')" data-estado="<?php echo $estado ?>">
+			<a href="<?php the_permalink(); ?>">
+				<?php $icon = get_field('icone_promocao'); ?>
+				<?php if ($icon){ ?>
+					<img src="<?php echo  $icon['url'] ?>" alt="<?php echo  $icon['alt']?>" class="icon_rest_promo"/><?php }
+				 ?>
+				<div class="resumobeneficio">
+					<h2><?php the_title() ?></h2>
+					<h3><?php the_field('nome_completo_do_hotel') ?></h3>
 				</div>
 			</a>
-	</div>
-<? } ?>
-		<?php endforeach; ?>
-		<?php wp_reset_postdata(); ?>
-		<?php endif; ?>
+		</div>
+	<?php 
+		$count_multiplo++;
+		endwhile; 
+	?>
+	<?php 
+		$htmlEspaco = '<div class="col-lg-4 col-md-4 col-sm-12 filtr-item <?php the_title(); ?>" 
+						style="background:url(/cookloversleclub/wp-content/uploads/2017/06/Restaurant_em_breve.jpg)"><a href="#"><div class="resumobeneficio"><h2>Aguarde</h2><h3>Em breve novos benefícios</h3></div></a></div>';
+		$count_multiplo = $count_multiplo % 3;
+		if( $count_multiplo == 1 ){
+			echo $htmlEspaco . $htmlEspaco;
+		}if($count_multiplo == 2){ 
+			echo $htmlEspaco;
+		};
+	?>
 	</div>
 </section>
 
